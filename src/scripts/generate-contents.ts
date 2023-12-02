03b150fe1abd92604fb9731bb52bb6c9519a8397
Ethan Halko <ethan@ethanhalko.com>
2023-12-02 15:15:39 -0500

    Refactor generator, make it look nicer

---
+++ src/scripts/generate-contents.ts
@@ -0,0 +1,51 @@
+import 'fs';
+import * as fs from 'fs';
+import {marked} from 'marked';
+
+type Blog = Record<string, string | Date>[][];
+
+const CHUNK = 10;
+const BASE_PATH = './posts';
+
+function getTitle(file: string) {
+  const titleLine = file.slice(file.indexOf('[title]'), file.indexOf('\n'))
+  return titleLine.replace('[title]', '');
+}
+
+function writePages(pages: Blog) {
+  pages.forEach((page, index) => {
+    fs.promises.writeFile(`./src/pages/${index + 1}.json`, JSON.stringify(page));
+  });
+
+  fs.promises.writeFile(`./src/pages/manifest.json`, JSON.stringify({ page_count: pages.length }));
+}
+
+export const generateContents = () => {
+  return {
+    name: 'generate-contents',
+    async buildStart() {
+      const dir = await fs.promises.readdir(BASE_PATH);
+      const pages: Blog = Array(Math.ceil(dir.length / CHUNK)).fill([]);
+
+      try {
+        for (let i = 0; i < dir.length; ++i) {
+          const path = BASE_PATH + '/' + dir[i];
+          const file = await fs.promises.readFile(path, { encoding: 'utf8' });
+
+          const {birthtime, mtime} = await fs.promises.stat(path);
+
+          pages[Math.floor(i / CHUNK)].push({
+            title: getTitle(file),
+            content: await marked.parse(file.slice(file.indexOf('\n'))),
+            created_at: birthtime,
+            updated_at: mtime
+          });
+        }
+
+        writePages(pages);
+      } catch(e) {
+        console.error(e);
+      }
+    }
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
-import { generateManifest } from './src/scripts/generate-manifest';
+import { generateContents } from './src/scripts/generate-contents';
 import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    generateManifest(),
+    generateContents(),
     Inspect(),
     UnoCSS()
   ],
