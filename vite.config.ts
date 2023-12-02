9c7a9149e069050c9cbd3efd15c1248956aa7249
Ethan Halko <ethan@ethanhalko.com>
2023-12-02 15:29:39 -0500

    Generated pages were supposed to be ignored

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import UnoCSS from 'unocss/vite'
 import { generateContents } from './src/scripts/generate-contents';
-import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
     generateContents(),
-    Inspect(),
+    react(),
     UnoCSS()
   ],
 })
